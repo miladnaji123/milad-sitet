2b77560cd23bccc51ebf4a866130ebd81c2ad7d7
miladnaji123 <34491327+miladnaji123@users.noreply.github.com>
2026-07-18 12:49:52 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,5 @@
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open('site-cache').then(function (cache) {
-      return cache.addAll([
-        '/milad-sitet/',
-        '/milad-sitet/index.html'
-      ]);
-    })
-  );
+self.addEventListener('install', e => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
-    })
-  );
-});
+self.addEventListener('fetch', e => {});
