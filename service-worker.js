2d914cf64f82ca6fa9e803665f2fadc9f84bf1a9
miladnaji123 <34491327+miladnaji123@users.noreply.github.com>
2026-07-19 16:50:59 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,23 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
+const CACHE_NAME = "calm-app-v1";
+
+const urlsToCache = [
+  "/milad-sitet/",
+  "/milad-sitet/index.html",
+  "/milad-sitet/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
